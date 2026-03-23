fe72876a97734450d5708bdba5f6b5213c79ab53
donfreddy <freddytamwo@gmail.com>
2026-03-23 16:28:41 +0100

    chore: update app configuration and favicon for improved branding

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -1,12 +1,21 @@
 export default defineAppConfig({
+  header: {
+    title: "Qora",
+    logo: {
+      // light: "/logo/logo-dark.svg",
+      // dark: "/logo/logo-light.svg",
+      // alt: "Qora Logo",
+      favicon: "/favicon.ico",
+    },
+  },
   ui: {
     colors: {
-      primary: 'purple',
-      neutral: 'zinc'
-    }
+      primary: "purple",
+      neutral: "zinc",
+    },
   },
   socials: {
-    discord: 'https://discord.gg/meragix',
-    x : 'https://x.com/meragix',
+    discord: "https://discord.gg/meragix",
+    x: "https://x.com/meragix",
   },
-})
+});
